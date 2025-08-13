a1a9e00b5164db4cbc67f757d667555bc0e980dc
hiroshi0918 <terahiro0918@gmail.com>
2025-08-13 12:15:23 +0900

    fix: add KeyboardAvoidingView.jsx

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,23 +1,44 @@
 import { 
-    View, TextInput, StyleSheet, KeyboardAvoidingView
+    View, TextInput, StyleSheet
 } from 'react-native'
 import { router } from 'expo-router'
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { useState} from 'react'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import { db, auth } from '../../config'
 
-const handlePress = (): void => {
-    // メモを保存する処理
+const handlePress = (bodyText: string): void => {
+    if (auth.currentUser) {
+        const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+        addDoc(ref, {
+            bodyText,
+            updatedAt: Timestamp.fromDate(new Date()),
+        })
+            .then((docRef) => {
+            console.log('Memo saved', docRef.id)
+        })
+        .catch((error) => {
+            console.error('Error saving memo:', error)
+        })
     router.back()
-}
+}}
 
 const Create = (): JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return (
-        <KeyboardAvoidingView behavior='height' style={styles.container}>
+        <KeyboardAvoidingView style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value='' />
+                <TextInput 
+                multiline 
+                style={styles.input} 
+                value={bodyText} 
+                onChangeText={setBodyText}
+                autoFocus />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => { handlePress(bodyText) }}>
                 <Icon name='check' size={40} color='#ffffff' />
             </CircleButton>
         </KeyboardAvoidingView>
